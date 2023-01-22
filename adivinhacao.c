6438f03c210ef7053da31a3d8a82d03459d91312
brunocnog <brunocesar.br@gmail.com>
2023-01-21 21:49:10 -0300

    Criando loops
    Definindo quantas vezes o jogador pode chutar um número.

--- adivinhacao.c
+++ adivinhacao.c
@@ -10,25 +10,28 @@ int main(int argc, char const *argv[])
     printf("* Bem-vindo ao Jogo da Adivinhação *\n");
     printf("************************************\n");
 
-    int numsecreto, chute;
+    int numsecreto=42, chute;
+    
+    for (int i = 1; i <= 3; i++){ //jogador tem 3 chances de acertar
+        printf("Qual é o seu %d° chute? ", i);
+        scanf("%d", &chute);
+        printf("Seu %d° chute foi %d!\n", i, chute);
 
-    numsecreto = 42;
-    printf("Qual é o seu chute? ");
-    scanf("%d", &chute);
-    printf("Seu chute foi %d!\n", chute);
-
-    int acertou = chute == numsecreto;
-
-    if(acertou){
-        printf("Parabéns! Você acertou!");
-    } else{        
-        int maior = chute >numsecreto;
-        if (maior){
-            printf("Seu chute foi maior que o número secreto!\n");
-        } else {
-            printf("Seu chute foi menor que o número secreto!\n");
-        }        
+        int acertou = chute == numsecreto;
+        if(acertou){ //verificando se o jogador acertou, se o chute e maior o menor que o número secreto
+            printf("Parabéns! Você acertou!\n\n");
+        } else{        
+            int maior = chute >numsecreto;
+            if (maior){
+                printf("Seu chute foi maior que o número secreto!\n");
+                printf("Tente Novamente!!\n\n");
+            } else {
+                printf("Seu chute foi menor que o número secreto!\n");
+                printf("Tente Novamente!!\n\n");
+            }        
+        }
     }
+    printf("Fim de Jogo!\n");
     return 0;
 }
 
