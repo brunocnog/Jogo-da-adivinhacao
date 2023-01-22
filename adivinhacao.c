fec9e728f93f3afb9eb86b0a7a7388fac77b2467
brunocnog <brunocesar.br@gmail.com>
2023-01-21 21:53:14 -0300

    add break
    Parando o jogo quando o jogador acertar o número secreto.

--- adivinhacao.c
+++ adivinhacao.c
@@ -20,6 +20,7 @@ int main(int argc, char const *argv[])
         int acertou = chute == numsecreto;
         if(acertou){ //verificando se o jogador acertou, se o chute e maior o menor que o número secreto
             printf("Parabéns! Você acertou!\n\n");
+            break;
         } else{        
             int maior = chute >numsecreto;
             if (maior){
